71420bd0220023caae20b1d83e555559bea9b2ff
Newton Ni <nwtnni@gmail.com>
2017-09-20 22:08:24 -0400

    Forgot a bunch of return statements. Thanks OCaml

--- util.py
+++ util.py
@@ -4,15 +4,15 @@ import shutil
 from zipfile import ZipFile
 from functools import reduce
 
-def exists(f): os.path.exists(f)
+def exists(f): return os.path.exists(f)
 
 def add_ext(f, ext): return f + ext
 
-def path(f): os.path.join(os.getcwd(), f)
+def path(f): return os.path.join(os.getcwd(), f)
 
 def to_path(s, root): return reduce(os.path.join, s.split("/"), root)
 
-def join(root, f): os.path.join(root, f)
+def join(root, f): return os.path.join(root, f)
 
 def is_dir(f): return os.path.isdir(f)
 
